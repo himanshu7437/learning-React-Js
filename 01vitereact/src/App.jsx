0fb829bcee99bb97b06726a5e3462f57a99966fa
himanshu7437 <himanshujangra368@gmail.com>
2025-03-01 01:54:26 +0530

    01vitereact added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai"
+
+function App() {
+
+  return (
+    // fragment (<></>) 
+    // keep the component capitalize 
+    <> 
+    <h1>Chai aur React with | Himanshu Sharma</h1>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai() {
+    return(
+        <>
+        <h2>hello mai hu chai</h2>
+        </>
+    );
+}
+
+export default Chai
