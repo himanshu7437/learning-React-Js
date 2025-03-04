adc4e4247847fd0e33165b08503bfe97c1022882
himanshu7437 <himanshujangra368@gmail.com>
2025-03-04 19:32:40 +0530

    using a new custom function and using it

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// create a function
+function MyApp() {
+  return(
+
+    <div>
+      <h1>CUSTOM APP!</h1>
+    </div>
+  )
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* using that function */}
+    <MyApp /> 
   </StrictMode>,
 )
