062843d4ba962defbbe512187f13f0019a16ff96
himanshu7437 <himanshujangra368@gmail.com>
2025-03-06 04:48:24 +0530

    leaming building components, props, handling props completed

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Cards from './Components/cards'
 
 function App() {
+
+  let myObj = {
+    username: "Himanshu",
+    age: "21",
+  }
 
   return (
     
     <>
-    <h1 className='bg-purple-500 text-black p-4 rounded-xl'>hello world</h1>
+    <h1 className='bg-purple-500 text-black p-4 rounded-xl mb-4'>hello world</h1>
+    <div className="cards flex gap-6">
+      <Cards username = "Chai aur Code" btnText = "Visit me" />
+      <Cards username = "Himanshu Sharma" />
+    </div>
     </>
   )
 }
