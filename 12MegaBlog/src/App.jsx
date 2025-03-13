c460983f0f7db401e3ec5fe1d11c1b21e27b52c2
himanshu7437 <himanshujangra368@gmail.com>
2025-03-14 01:02:38 +0530

    include auth.js, tailwind, header, footer

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,43 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import { Footer, Header } from './components'
+import {Outlet} from 'react-dom'
+
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
 
 
-  return (
-    <>
-    <h1>hello this our mega project</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='flex flex-wrap content-between min-h-screen bg-gray-400'>
+      <div className='block w-full'>
+        <Header />
+        <main>
+          Todo:{/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+    
+  ): null
 }
 
 export default App
