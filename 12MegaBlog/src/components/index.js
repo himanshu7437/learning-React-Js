7c04b6812724053c103456e3f37cbc32caebd0d3
himanshu7437 <himanshujangra368@gmail.com>
2025-03-16 08:15:08 +0530

    rte and post form added

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -2,6 +2,8 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import LogoutBtn from './Header/LogoutBtn'
+import RTE from "./RTE";
 
 
-export {Header, Footer, Container, Logo}
+export {Header, Footer, Container, Logo, LogoutBtn, RTE }
